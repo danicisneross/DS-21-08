1a6865040cdaf97f02a04007a0c7be65dbba4918
Daniela <83793846+danicisneross@users.noreply.github.com>
2021-11-12 11:33:50 +0100

    Update Guardabosques.java

--- DS-21-08-B2/src/e1/Guardabosques.java
+++ DS-21-08-B2/src/e1/Guardabosques.java
@@ -2,19 +2,6 @@ package e1;
 
 public class Guardabosques extends Personal {
 
-    /**
-     * Constructor de la clase Guardabosques.
-     *
-     * @param nombre      datos del miembro.
-     * @param apellido    datos del miembro.
-     * @param edad        datos del miembro.
-     * @param horrocruxes numero de horrocruxes destruidos.
-     *                    Los galeones por horrocrux (heredados de Member) dependen de cada clase. Los
-     *                    utilizaremos para calcular la recompensa_anual.
-     *                    Tenemos el salario_base y la compensacion por nocturnidad inicializadas
-     *                    que heredamos de Personal utilizadas para calcular el salario.
-     */
-
     public Guardabosques(String nombre, String apellido, int edad, int horrocruxes) {
         super(nombre, apellido, edad, horrocruxes);
         setGaleones(75);
