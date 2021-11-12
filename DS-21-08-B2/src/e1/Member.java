861a0733d42f696c24f918c34d479ffae7ca666c
Daniela <83793846+danicisneross@users.noreply.github.com>
2021-11-12 11:16:16 +0100

    Update Member.java

--- DS-21-08-B2/src/e1/Member.java
+++ DS-21-08-B2/src/e1/Member.java
@@ -21,16 +21,7 @@ public abstract class Member {
             throw new IllegalArgumentException();
         }
     }
-
-    /**
-     * recompensa_anual es un metodo comun aplicable a todos los miembros, donde el Ministerio de
-     * Magia otorga una recompensa dependiendo de los horrocruxes destruidos y de la clase a
-     * a la que pertenezca.
-     *
-     * @return recompensa.
-     */
-
-
+    
     public String getNombre() {
         return nombre;
     }
