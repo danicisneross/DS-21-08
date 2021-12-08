ac777c24d3d98d96e275fa6a313f3630e58ba04d
mauro <mauro.moranm@udc.es>
2021-12-08 18:22:02 +0100

    Ejercicio 1 PD falta principio abierto cerrado

--- DS-21-08-PD/src/e1/Gestion.java
+++ DS-21-08-PD/src/e1/Gestion.java
@@ -2,11 +2,11 @@ package e1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 public class Gestion {
-    private List<Billete> listabilletes = new ArrayList<>();// = new ArrayList<>();
-    //public List<Billete> billetesseleccionados;// = new ArrayList<>();
+    private List<Billete> listabilletes = new ArrayList<>();
 
     private final String origen1;
     private final String origen2;
@@ -42,84 +42,102 @@ public class Gestion {
         this.puertaF = puertaF;
     }
 
-    IteradorLista iteradorLista = new IteradorLista(listabilletes,0);
-
     public void addBillete (String origen, String destino, float precio, int fecha) {
         Billete billete = new Billete(origen, destino, precio, fecha);
         listabilletes.add(billete);
     }
 
+    IteradorLista iteradorLista = new IteradorLista(listabilletes,0);
+
     public Gestion hacerbusqueda(String origen1, String origen2, Logica puertaO, String destino1, String destino2,
                                  Logica puertaD, float precio1, float precio2, Logica puertaP, int fecha1, int fecha2,
                                  Logica puertaF) {
-        Gestion busqueda = new Gestion(origen1, origen2, puertaO, destino1, destino2, puertaD, precio1, precio2, puertaP,
+        return new Gestion(origen1, origen2, puertaO, destino1, destino2, puertaD, precio1, precio2, puertaP,
                 fecha1, fecha2, puertaF);
-        return busqueda;
     }
 
-    public List<Billete> resultadoBusqueda (Gestion busqueda) {
+    public List<Billete> resultadoBusqOrigen (Gestion busqueda) {
         List<Billete> billetesSeleccionados = new ArrayList<>();
         int index = 0;
         if (busqueda.puertaO == null) {
             while (iteradorLista.hasNext()) {
-                if (busqueda.origen1 == listabilletes.get(index).getOrigen())
+                if (Objects.equals(busqueda.origen1, listabilletes.get(index).getOrigen()))
                     billetesSeleccionados.add(listabilletes.get(index));
+                iteradorLista.next();
+            }
+        } else if (busqueda.puertaO == Logica.OR) {
+            while (iteradorLista.hasNext()) {
+                if (Objects.equals(busqueda.origen1, listabilletes.get(index).getOrigen()) ||
+                        Objects.equals(busqueda.origen2, listabilletes.get(index).getOrigen()))
+                    billetesSeleccionados.add(listabilletes.get(index));
+                iteradorLista.next();
+            }
+        } else if (busqueda.puertaO == Logica.AND || busqueda.puertaD == Logica.AND || busqueda.puertaP == Logica.AND
+                || busqueda.puertaF == Logica.AND)
+            return null;
+        return billetesSeleccionados;
+    }
+
+    public List<Billete> resultadoBusqDestino (Gestion busqueda, List<Billete> billetesOrigen) {
+        List<Billete> billetesSeleccionados = new ArrayList<>();
+        int index = 0;
+        IteradorLista iteradorLista1 = new IteradorLista(billetesOrigen, index);
+        if (busqueda.puertaD == null) {
+            while (iteradorLista1.hasNext()) {
+                if (Objects.equals(busqueda.destino1, billetesOrigen.get(index).getDestino()))
+                    billetesSeleccionados.add(billetesOrigen.get(index));
+                iteradorLista1.next();
+            }
+        } else if (busqueda.puertaD == Logica.OR) {
+            while (iteradorLista1.hasNext()) {
+                if (Objects.equals(busqueda.destino1, billetesOrigen.get(index).getDestino()) ||
+                        Objects.equals(busqueda.destino2, billetesOrigen.get(index).getDestino()))
+                    billetesSeleccionados.add(billetesOrigen.get(index));
+                iteradorLista1.next();
             }
         }
         return billetesSeleccionados;
     }
 
-
-/*
-    public List<Billete> busquedaOrigen(Logica puerta, String origen1, String origen2) {
-        if(puerta == null) {
-            while (iteradorLista.hasNext()) {
-                if ()
+    public List<Billete> resultadoBusqPrecio (Gestion busqueda, List<Billete> billetesOrigenyDestino) {
+        List<Billete> billetesSeleccionados = new ArrayList<>();
+        int index = 0;
+        IteradorLista iteradorLista2 = new IteradorLista(billetesOrigenyDestino, index);
+        if (busqueda.puertaP == null) {
+            while (iteradorLista2.hasNext()) {
+                if (Objects.equals(busqueda.precio1, billetesOrigenyDestino.get(index).getPrecio()))
+                    billetesSeleccionados.add(billetesOrigenyDestino.get(index));
+                iteradorLista2.next();
             }
-        } else if(puerta == Logica.OR) {
-
-        } else return null;
-        return billetesseleccionados;
-    }
-
-    public List<Billete> busquedaDestino(Logica puerta, String destino1, String destino2) {
-        if(puerta == null) {
-            while (iteradorLista.hasNext()) {
-                if ()
+        } else if (busqueda.puertaP == Logica.OR) {
+            while (iteradorLista2.hasNext()) {
+                if (Objects.equals(busqueda.precio1, billetesOrigenyDestino.get(index).getPrecio()) ||
+                        Objects.equals(busqueda.precio2, billetesOrigenyDestino.get(index).getPrecio()))
+                    billetesSeleccionados.add(billetesOrigenyDestino.get(index));
+                iteradorLista2.next();
             }
-        } else if(puerta == Logica.OR) {
-
-        } else return null;
-        return billetesseleccionados;
-    }
-
-    public List<Billete> busquedaPrecio(Logica puerta, float precio1, float precio2) {
-        if(puerta == null) {
-            while (iteradorLista.hasNext()) {
-                if ()
-            }
-        } else if(puerta == Logica.AND) {
-
-        } else if(puerta == Logica.OR) {
-
         }
-        return billetesseleccionados;
+        return billetesSeleccionados;
     }
 
-    public List<Billete> busquedaFecha(Logica puerta, int fecha1, int fecha2) {
-        if(puerta == null) {
-            while (iteradorLista.hasNext()) {
-                if ()
+    public List<Billete> resultadoBusqFecha (Gestion busqueda, List<Billete> billetesOrigenyDestinoyPrecio) {
+        List<Billete> billetesSeleccionados = new ArrayList<>();
+        int index = 0;
+        IteradorLista iteradorLista3 = new IteradorLista(billetesOrigenyDestinoyPrecio, index);
+        if (busqueda.puertaF == null) {
+            while (iteradorLista3.hasNext()) {
+                if (Objects.equals(busqueda.fecha1, billetesOrigenyDestinoyPrecio.get(index).getFecha()))
+                    billetesSeleccionados.add(billetesOrigenyDestinoyPrecio.get(index));
+                iteradorLista3.next();
             }
-        } else if(puerta == Logica.AND) {
-
-        } else if(puerta == Logica.OR) {
-
+        } else if (busqueda.puertaF == Logica.OR) {
+            while (iteradorLista3.hasNext()) {
+                if (Objects.equals(busqueda.fecha1, billetesOrigenyDestinoyPrecio.get(index).getFecha()) ||
+                        Objects.equals(busqueda.fecha2, billetesOrigenyDestinoyPrecio.get(index).getFecha()))
+                    billetesSeleccionados.add(billetesOrigenyDestinoyPrecio.get(index));
+                iteradorLista3.next();
+            }
         }
-        return billetesseleccionados;
+        return billetesSeleccionados;
     }
-
- */
 }
-
-
